c5d9f43b5fd8f66cf256dd72c1a4c6c0df0cedbe
Haesun Park <haesunrpark@gmail.com>
2022-04-02 23:48:47 +0900

    16장 추가

---
+++ ch16/data.js
@@ -0,0 +1,125 @@
+const IMAGE_SIZE = 784;
+const NUM_CLASSES = 10;
+const NUM_DATASET_ELEMENTS = 65000;
+
+const TRAIN_TEST_RATIO = 5 / 6;
+
+const NUM_TRAIN_ELEMENTS = Math.floor(TRAIN_TEST_RATIO * NUM_DATASET_ELEMENTS);
+const NUM_TEST_ELEMENTS = NUM_DATASET_ELEMENTS - NUM_TRAIN_ELEMENTS;
+
+const MNIST_IMAGES_SPRITE_PATH =
+    'https://storage.googleapis.com/learnjs-data/model-builder/mnist_images.png';
+const MNIST_LABELS_PATH =
+    'https://storage.googleapis.com/learnjs-data/model-builder/mnist_labels_uint8';
+
+/**
+ * 스프라이트된 MNIST 데이터셋을 가져와 셔플된 배치를 반환하는 클래스
+ *
+ * 노트: 더 쉽게 만들 수 있지만 지금은 데이터 다운로드와 조작을 수동으로 합니다.
+ */
+export class MnistData {
+    constructor() {
+        this.shuffledTrainIndex = 0;
+        this.shuffledTestIndex = 0;
+    }
+
+    async load() {
+        // MNIST 스프라이트 이미지를 요청합니다.
+        const img = new Image();
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        const imgRequest = new Promise((resolve, reject) => {
+            img.crossOrigin = '';
+            img.onload = () => {
+                img.width = img.naturalWidth;
+                img.height = img.naturalHeight;
+
+                const datasetBytesBuffer =
+                    new ArrayBuffer(NUM_DATASET_ELEMENTS * IMAGE_SIZE * 4);
+
+                const chunkSize = 5000;
+                canvas.width = img.width;
+                canvas.height = chunkSize;
+
+                for (let i = 0; i < NUM_DATASET_ELEMENTS / chunkSize; i++) {
+                    const datasetBytesView = new Float32Array(
+                        datasetBytesBuffer, i * IMAGE_SIZE * chunkSize * 4,
+                        IMAGE_SIZE * chunkSize);
+                    ctx.drawImage(
+                        img, 0, i * chunkSize, img.width, chunkSize, 0, 0, img.width,
+                        chunkSize);
+
+                    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+                    for (let j = 0; j < imageData.data.length / 4; j++) {
+                        // 흑백 이미지이므로 모든 채널의 값이 같습니다.
+                        // 따라서 그냥 빨강 채널의 값을 읽습니다.
+                        datasetBytesView[j] = imageData.data[j * 4] / 255;
+                    }
+                }
+                this.datasetImages = new Float32Array(datasetBytesBuffer);
+
+                resolve();
+            };
+            img.src = MNIST_IMAGES_SPRITE_PATH;
+        });
+
+        const labelsRequest = fetch(MNIST_LABELS_PATH);
+        const [imgResponse, labelsResponse] =
+            await Promise.all([imgRequest, labelsRequest]);
+
+        this.datasetLabels = new Uint8Array(await labelsResponse.arrayBuffer());
+
+        // 랜덤한 훈련 세트와 검증 세트를 만들기 위해 셔플 인덱스를 만듭니다.
+        this.trainIndices = tf.util.createShuffledIndices(NUM_TRAIN_ELEMENTS);
+        this.testIndices = tf.util.createShuffledIndices(NUM_TEST_ELEMENTS);
+
+        // 이미지와 레이블을 훈련 세트와 테스트 세트로 나눕니다.
+        this.trainImages =
+            this.datasetImages.slice(0, IMAGE_SIZE * NUM_TRAIN_ELEMENTS);
+        this.testImages = this.datasetImages.slice(IMAGE_SIZE * NUM_TRAIN_ELEMENTS);
+        this.trainLabels =
+            this.datasetLabels.slice(0, NUM_CLASSES * NUM_TRAIN_ELEMENTS);
+        this.testLabels =
+            this.datasetLabels.slice(NUM_CLASSES * NUM_TRAIN_ELEMENTS);
+    }
+
+    nextTrainBatch(batchSize) {
+        return this.nextBatch(
+            batchSize, [this.trainImages, this.trainLabels], () => {
+                this.shuffledTrainIndex =
+                    (this.shuffledTrainIndex + 1) % this.trainIndices.length;
+                return this.trainIndices[this.shuffledTrainIndex];
+            });
+    }
+
+    nextTestBatch(batchSize) {
+        return this.nextBatch(batchSize, [this.testImages, this.testLabels], () => {
+            this.shuffledTestIndex =
+                (this.shuffledTestIndex + 1) % this.testIndices.length;
+            return this.testIndices[this.shuffledTestIndex];
+        });
+    }
+
+    nextBatch(batchSize, data, index) {
+        const batchImagesArray = new Float32Array(batchSize * IMAGE_SIZE);
+        const batchLabelsArray = new Uint8Array(batchSize * NUM_CLASSES);
+
+        for (let i = 0; i < batchSize; i++) {
+            const idx = index();
+
+            const image =
+                data[0].slice(idx * IMAGE_SIZE, idx * IMAGE_SIZE + IMAGE_SIZE);
+            batchImagesArray.set(image, i * IMAGE_SIZE);
+
+            const label =
+                data[1].slice(idx * NUM_CLASSES, idx * NUM_CLASSES + NUM_CLASSES);
+            batchLabelsArray.set(label, i * NUM_CLASSES);
+        }
+
+        const xs = tf.tensor2d(batchImagesArray, [batchSize, IMAGE_SIZE]);
+        const labels = tf.tensor2d(batchLabelsArray, [batchSize, NUM_CLASSES]);
+
+        return {xs, labels};
+    }
+}
